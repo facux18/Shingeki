176e6d4313bb146cffe847bb81e8b1b5390e8563
Facu Martinez <mc_webb@hotmail.com.ar>
2022-01-11 20:24:38 -0300

    pagina titanes
    se agrega pagina de titanes

--- pages/Characters/Character.jsx
+++ pages/Characters/Character.jsx
@@ -1,4 +1,5 @@
 export default function Character() {
+    
     return (
         <div>
             <h1>Titan colosal 2 re loco</h1>
---
+++ pages/Titanes/Titans.jsx
@@ -0,0 +1,8 @@
+export default function Titanes() {
+console.log("nose")
+    return (
+        <div>
+            <h1>Titanes</h1>
+        </div>
+    )
+}
